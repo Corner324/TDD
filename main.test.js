b73d692708d156cdc6bb83204fbf450ba7559756
Александр <mosenn@mail.ru>
2023-10-05 18:50:22 +0300

    Additional tests

--- main.test.js
+++ main.test.js
@@ -1,27 +1,63 @@
 const {Converter} = require("./main")
 
-describe('Обмен валют', () => {
-    it('конвертируем 100 долларов в евро', () => {
-      const amount = 100;
-      const fromCurrency = 'USD';
-      const toCurrency = 'EUR';
-      const expectedAmount = 85;
-        
-      const con = new Converter()
-      const result = con.exchange(mount, fromCurrency, toCurrency)
-  
-      expect(result).toBe(expectedAmount);
-    });
-    it('конвертируем 70 долларов в евро', () => {
-        const amount = 70;
+describe('Работа с валютами', () => {
+    describe('Обмен валют', () => {
+        it('конвертируем 100 долларов в евро', () => {
+        const amount = 100;
         const fromCurrency = 'USD';
         const toCurrency = 'EUR';
         const expectedAmount = 85;
-          
+            
         const con = new Converter()
         const result = con.exchange(mount, fromCurrency, toCurrency)
     
         expect(result).toBe(expectedAmount);
-      });
+        });
+        it('конвертируем 70 долларов в евро', () => {
+            const amount = 70;
+            const fromCurrency = 'USD';
+            const toCurrency = 'EUR';
+            const expectedAmount = 85;
+            
+            const con = new Converter()
+            const result = con.exchange(mount, fromCurrency, toCurrency)
+        
+            expect(result).toBe(expectedAmount);
+        });
+        it('конвертируем 100 евро в доллары', () => {
+            const amount = 100;
+            const fromCurrency = 'EUR';
+            const toCurrency = 'USD';
+            const expectedAmount = 85;
+            
+            const con = new Converter()
+            const result = con.exchange(mount, fromCurrency, toCurrency)
+        
+            expect(result).toBe(expectedAmount);
+        });
+        it('конвертируем 80 рублей в евро', () => {
+            const amount = 80;
+            const fromCurrency = 'RUB';
+            const toCurrency = 'EUR';
+            const expectedAmount = 85;
+            
+            const con = new Converter()
+            const result = con.exchange(mount, fromCurrency, toCurrency)
+        
+            expect(result).toBe(expectedAmount);
+        });
+    })
 
-})
+    describe('Предсказание курса', () => {
+        it('предсказываем дальнейший курс евро', () => {
+            const сurrency = 'EUR';
+            const expectedAmount = [20,30,40];
+
+            const result = con.predict(сurrency)
+            expect(result).toBe(expectedAmount);
+
+        });
+    });
+
+
+});
